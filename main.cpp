c36df23deb1a8a1f6e89ddcd986797f2677ef1e4
Tanmayee Chalamalasetti <tanmayeesc@Tanmayees-MacBook-Pro-190.local>
2025-09-28 21:38:25 -0700

    Push 5; back from break

--- main.cpp
+++ main.cpp
@@ -15,6 +15,30 @@ class Movie {
   void setYR(int y) { yearReleased = y; }
   string getTitle() { return title; }
   void setGreen(int t) { title = t; }
-};
 
-int main() { return 0; }
+  void print() {
+    cout << title << ": (" << red << ", " << green << ", " << blue << ")\n";
+  }
+};
+}
+;
+
+int main() {
+  Movie burgundy;
+  // Create and initialize the Color burgundy
+  burgundy.setName("Burgundy");
+  burgundy.setRed(128);
+  burgundy.setGreen(0);
+  burgundy.setBlue(32);
+  burgundy.print();  // Displays Color burgundy
+
+  // Create and initialize the Color viridian
+  Movie viridian;
+  viridian.setName("Viridian");
+  viridian.setRed(64);
+  viridian.setGreen(130);
+  viridian.setBlue(109);
+  viridian.print();  // Displays Color viridian
+
+  return 0;
+}
